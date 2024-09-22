3111579ea013dfe4c0b0061d83e4e19763dae0a4
Asus Vivobook <mod326432@yahoo.com>
2024-09-22 18:35:19 +0200

    Create the python library and constant parameters

---
+++ Main.py
@@ -0,0 +1,21 @@
+import os
+from tqdm import tqdm
+import numpy as np
+import random
+#from utils import *
+
+
+
+# constant parameters
+spilt = True
+input_size = 256
+use_network = False
+filter_lenght = 32
+kernel_size = 16
+drop_rate = 0.2
+feature = 'MLII'
+epoch = 10
+batch = 256
+
+
+
